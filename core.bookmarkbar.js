238470fd65c803e76aa855a132a69705e331439a
Michael Hart <michaelhart@michaelhart.me>
2011-11-17 17:04:58 -0500

    initial commit

---
+++ core.bookmarkbar.js
@@ -0,0 +1,78 @@
+/*
+  Original code by Sachleen (sachleen.com // sachleen.sandhu@gmail.com)
+  https://chrome.google.com/webstore/detail/feckadlaijbceoglkncdgebgkminbkia
+
+  New Tab With Clock by Sachleen Sandhu is licensed under a Creative
+  Commons Attribution-ShareAlike 3.0 Unported License.
+  http://creativecommons.org/licenses/by-sa/3.0/
+*/
+
+/**
+* Displays the bookmarks bar.
+*
+* @param {BookmarkTreeNode array} array of BookmarkTreeNode results
+*/
+
+chrome.bookmarks.getTree(getBookmarks);
+function getBookmarks(tree)
+{
+    //bookmarksList = '<ul id="nav">';
+    bookmarksList = '<li id="other"><a href="#"><img class="favicon" src="core.bookmarkbar.folder.png" title="Other"> Other Bookmarks   </a><ul>';
+    buildTree(tree[0].children[1]);
+    bookmarksList += '</ul></li>';
+    buildTree(tree[0].children[0]);
+    //bookmarksList += '</ul>';
+    $('#bookmarksBar').html(bookmarksList);
+}
+
+function buildTree(items)
+{
+    var items = items.children;
+    for (var i = 0; i < items.length; i++)
+    {
+        if(items[i].url != undefined) {
+            if( (items[i].url).indexOf("javascript") === -1 ) {
+            bookmarksList += '<li><a href="{0}"><img height="16px" width="16px" class="favicon" src="chrome://favicon/{0}" title="{1}">{1}</a></li>'.format(items[i].url, items[i].title != "" ? " " + truncate(items[i].title, 23, true) : "");
+            }
+        }
+        else
+        {
+            bookmarksList += '<li>';
+            bookmarksList += '<a href="#"><img height="16px" width="16px" class="favicon" src="core.bookmarkbar.folder.png" title="{0}">{0}</a>'.format(items[i].title != "" ? " " + truncate(items[i].title, 23, true) : "");
+            bookmarksList += '<ul>';
+            buildTree(items[i]);
+            bookmarksList += '</ul></li>';
+        }
+    }
+}
+
+/**
+* Truncates a string to specified length.
+*
+* @param {string} string to truncate
+* @param {int} maximum length of truncated string
+* @param {bool} If true, an ellipsis will be added to the end of the truncated string
+* @return {string} Retruns truncated string. If string is shorter than len, unmodified string is returned.
+*/
+function truncate(str, len, dots)
+{
+    var ret = str;
+    if(str.length > len)
+    {
+        ret = str.substring(0, len-1);
+        if(dots)
+            ret += "...";
+    }
+    
+    return ret;
+}
+
+String.prototype.format = function() {
+    var s = this,
+        i = arguments.length;
+
+    while (i--) {
+        s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
+    }
+    return s;
+};
---
+++ core.hscroll.js
@@ -0,0 +1,74 @@
+/* Copyright 2008 Paul Bennett - http://paulicio.us
+ * Scroller.js
+ * Captures mouse wheel events and runs the ScrollSmoothly
+ * function based on their output.
+ * Aims to aid usability by allowing the user to scroll the 
+ * page horizontally smoothly using only their mousewheel.
+ * Mousewheel event capture by Adomas Paltanavičius at http://adomas.org/
+ */
+
+var hscroll = true;
+$("#widget-drawer,#app-drawer,.edit-shortcut-ui,#options-ui").live({
+  mouseenter: function() {
+    hscroll = false;
+  },
+  mouseleave: function() {
+    hscroll = true;
+  }
+});
+
+
+
+
+function handle(delta) {
+        if (delta <0)
+                ScrollSmoothly(10,10,'right');
+        else if (delta >0)
+                ScrollSmoothly(10,10,'left');
+        else
+            ;
+}
+ 
+function wheel(event){
+  if(hscroll === false) {
+    return;
+  }
+        var delta = 0;
+        if (!event) 
+                event = window.event;
+        if (event.wheelDelta) {
+                delta = event.wheelDelta/120;
+                if (window.opera)
+                        delta = -delta;
+        } else if (event.detail) {
+                delta = -event.detail/3;
+        }
+        if (delta)
+                handle(delta);
+        if (event.preventDefault)
+                event.preventDefault();
+    event.returnValue = false;
+}
+
+var repeatCount = 0;
+
+function ScrollSmoothly(scrollPos,repeatTimes, direction) {
+    if(repeatCount < repeatTimes)
+        if(direction == 'right')
+            window.scrollBy(15,0);
+        else
+            window.scrollBy(-15,0);
+    else
+    {
+        repeatCount = 0;
+        clearTimeout(cTimeout);
+        return;
+    }
+    repeatCount++;
+    cTimeout = setTimeout("ScrollSmoothly('" + scrollPos + "','"+ repeatTimes +"','"+ direction +"')",10);
+}
+ 
+/* Initialization code. */
+if (window.addEventListener)
+        window.addEventListener('DOMMouseScroll', wheel, false);
+window.onmousewheel = document.onmousewheel = wheel;
