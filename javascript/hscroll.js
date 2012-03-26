9a092693b2654b0f537afe8653b7dbdf431c3735
Michael Hart <michaelhart@michaelhart.me>
2012-03-25 21:20:34 -0400

    Some cleanup and project consistency edits

--- javascript/hscroll.js
+++ javascript/hscroll.js
@@ -17,58 +17,54 @@ $("#widget-drawer,#app-drawer,.edit-shortcut-ui,#options-ui,.ui-2").live({
   }
 });
 
-
-
-
-function handle(delta) {
-        if (delta <0)
-                ScrollSmoothly(10,10,'right');
-        else if (delta >0)
-                ScrollSmoothly(10,10,'left');
-        else
-            ;
+ function handle(delta) {
+  if (delta <0)
+    ScrollSmoothly(10,10,'right');
+  else if (delta >0)
+    ScrollSmoothly(10,10,'left');
 }
- 
+
 function wheel(event){
   if(hscroll === false) {
     return;
   }
-        var delta = 0;
-        if (!event) 
-                event = window.event;
-        if (event.wheelDelta) {
-                delta = event.wheelDelta/120;
-                if (window.opera)
-                        delta = -delta;
-        } else if (event.detail) {
-                delta = -event.detail/3;
-        }
-        if (delta)
-                handle(delta);
-        if (event.preventDefault)
-                event.preventDefault();
-    event.returnValue = false;
+  var delta = 0;
+  if (!event) 
+    event = window.event;
+  if (event.wheelDelta) {
+    delta = event.wheelDelta/120;
+    if (window.opera)
+      delta = -delta;
+  } else if (event.detail) {
+    delta = -event.detail/3;
+  }
+  if (delta)
+    handle(delta);
+  if (event.preventDefault)
+    event.preventDefault();
+  event.returnValue = false;
 }
 
 var repeatCount = 0;
 
 function ScrollSmoothly(scrollPos,repeatTimes, direction) {
-    if(repeatCount < repeatTimes)
-        if(direction == 'right')
-            window.scrollBy(15,0);
-        else
-            window.scrollBy(-15,0);
+  if(repeatCount < repeatTimes)
+    if(direction == 'right')
+      window.scrollBy(15,0);
+    else
+      window.scrollBy(-15,0);
     else
     {
-        repeatCount = 0;
-        clearTimeout(cTimeout);
-        return;
+      repeatCount = 0;
+      clearTimeout(cTimeout);
+      return;
     }
     repeatCount++;
     cTimeout = setTimeout("ScrollSmoothly('" + scrollPos + "','"+ repeatTimes +"','"+ direction +"')",10);
-}
- 
-/* Initialization code. */
-if (window.addEventListener)
-        window.addEventListener('DOMMouseScroll', wheel, false);
-window.onmousewheel = document.onmousewheel = wheel;
+  }
+
+  /* Initialization code. */
+  if (window.addEventListener) {
+    window.addEventListener('DOMMouseScroll', wheel, false);
+  }
+  window.onmousewheel = document.onmousewheel = wheel;
