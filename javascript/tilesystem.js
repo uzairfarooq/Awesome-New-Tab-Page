2b2e958526a701f64d5803f466c29a50eb688ac3
Michael Hart <michaelhart@michaelhart.me>
2012-03-29 06:07:04 -0400

    Tilesystem cleanup & optimize

--- javascript/tilesystem.js
+++ javascript/tilesystem.js
@@ -16,13 +16,6 @@
  *
  */
 
-var GRID_MIN_HEIGHT   = 3,
-    GRID_MIN_WIDTH    = 7,
-    GRID_MARGIN_TOP   = function() { return localStorage.getItem("showbmb") === "yes" ? 27 : 0; },
-    GRID_MARGIN_LEFT  = 27,
-    GRID_TILE_SIZE    = 200,  /* NEVER CHANGE */
-    GRID_TILE_PADDING = 3;    /* NEVER CHANGE */
-
 $(document).ready(function($) {
   $("#toggle-grid").live("click", updateGridOpacity);
 
@@ -39,6 +32,14 @@ $(document).ready(function($) {
   placeWidgets();
 });
 
+var GRID_MIN_HEIGHT   = 3,
+    GRID_MIN_WIDTH    = 7,
+    GRID_MARGIN_TOP   = function() { return localStorage.getItem("showbmb") === "yes" ? 27 : 0; },
+    GRID_MARGIN_LEFT  = 27,
+    GRID_TILE_SIZE    = 200,  /* NEVER CHANGE */
+    GRID_TILE_PADDING = 3;    /* NEVER CHANGE */
+
+// Handles permanent grid preference
 function updateGridOpacity() {
   if ($("#toggle-grid").is(':checked')) {
     $("body").addClass("perm-grid");
@@ -51,6 +52,7 @@ function updateGridOpacity() {
   }
 }
 
+// Handles positioning and repositioning the grid
 function moveGrid(pref) {
   if ( pref.animate_top === false ) {
     $("#widget-holder,#grid-holder").css({
@@ -77,12 +79,13 @@ function placeGrid() {
   var height = GRID_MIN_HEIGHT;
   var width = GRID_MIN_WIDTH;
 
+  // Ensure window is filled with grid tiles
   if ( typeof(window.innerHeight) !== "undefined"
     && typeof(window.innerWidth) !== "undefined"
     && typeof(screen.width) !== "undefined" ) {
     var res_height = Math.floor( ( window.innerHeight - GRID_MARGIN_TOP() ) / ( GRID_TILE_SIZE + ( GRID_TILE_PADDING * 2 ) ) );
-    var res_width  = Math.floor( ( window.innerWidth  - GRID_MARGIN_LEFT )  / ( GRID_TILE_SIZE + ( GRID_TILE_PADDING * 2 ) ) ) + 3;
-    var res_width2 = Math.floor( ( screen.width       - GRID_MARGIN_LEFT )  / ( GRID_TILE_SIZE + ( GRID_TILE_PADDING * 2 ) ) ) + 3;
+    var res_width  = Math.floor( ( window.innerWidth  - GRID_MARGIN_LEFT  ) / ( GRID_TILE_SIZE + ( GRID_TILE_PADDING * 2 ) ) ) + 3;
+    var res_width2 = Math.floor( ( screen.width       - GRID_MARGIN_LEFT  ) / ( GRID_TILE_SIZE + ( GRID_TILE_PADDING * 2 ) ) ) + 3;
 
     if(res_height > height) {
       height = res_height;
@@ -95,15 +98,17 @@ function placeGrid() {
     }
   }
 
-  var widgets = JSON.parse(localStorage.getItem("widgets")),
-      placed_height = 0, placed_width = 0;
+  widgets = JSON.parse(localStorage.getItem("widgets"));
+  var placed_height = 0, placed_width = 0;
+
+  // Ensure all placed widgets have a grid tile to land on
   if( typeof(widgets) === "object" ) {
     $.each(widgets, function(id, widget) {
       if( parseFloat(widget.where[0]) + parseFloat(widget.size[0]) > placed_height ) {
         placed_height = parseFloat(widget.where[0]) + parseFloat(widget.size[0]);
       }
       if( parseFloat(widget.where[1]) + parseFloat(widget.size[1]) + 3 > placed_width ) {
-        placed_width = parseFloat(widget.where[1]) + parseFloat(widget.size[1]) + 3;
+        placed_width  = parseFloat(widget.where[1]) + parseFloat(widget.size[1]) + 3;
       }
     });
 
@@ -115,6 +120,7 @@ function placeGrid() {
     }
   }
 
+  // Actually place the grid
   for (var gx = 0; gx < height; gx++) {
     for (var gy = 0; gy < width; gy++) {
       $(tile_template).appendTo("#grid-holder").css({
@@ -123,7 +129,7 @@ function placeGrid() {
         "left": ( gy * GRID_TILE_SIZE ) + ( ( GRID_TILE_PADDING * 2 ) * ( gy + 1 ) )
       }).attr({
         "id": gx + "x" + gy,
-        "data-land-top": gx,
+        "data-land-top" : gx,
         "data-land-left": gy
       });
     }
@@ -151,17 +157,17 @@ function makeZero(num){
 
 function findClosest(tile){
   var closestElm = null;
-  var boxx = $(tile).position().left;
-  var boxy = $(tile).position().top;
+  var boxX = $(tile).position().left;
+  var boxY = $(tile).position().top;
   var distElm = -1;
   $(".tile").each(function(ind, elem){
-      var closex = $(elem).position().left;
-      var closey = $(elem).position().top;
-      testElm = Math.pow(boxx - closex, 2) + Math.pow(boxy - closey, 2);
-      if(testElm < distElm || distElm == -1){
-        distElm = testElm;
-        closestElm = elem;
-      }
+    var closeX = $(elem).position().left;
+    var closeY = $(elem).position().top;
+    testElm = Math.pow(boxX - closeX, 2) + Math.pow(boxY - closeY, 2);
+    if(testElm < distElm || distElm == -1){
+      distElm = testElm;
+      closestElm = elem;
+    }
   });
   return closestElm;
 }
@@ -185,10 +191,8 @@ function getCovered(tile) {
   var height = parseInt( $(tile).attr("data-tile-height")   , 10);
   var width  = parseInt( $(tile).attr("data-tile-width")    , 10);
 
-  var h=0;
   for (h=0; h<=(height-1); h++)
   {
-    var w=0;
     for (w=0; w<=(width-1); w++)
     {
       var temporary_tile = $("#"+(top+h)+"x"+(left+w)+".tile")[0];
@@ -211,210 +215,186 @@ function getCovered(tile) {
 
 function setStuff(){
   $(".widget").each(function(ind, elem){
-      $(elem).css("width",
-        $(elem).attr("data-tile-width") * 200 + (makeZero($(elem).attr("data-tile-width") - 1) * 6)
-      );
-      $(elem).css("height",
-        $(elem).attr("data-tile-height") * 200 + (makeZero($(elem).attr("data-tile-height") - 1) * 6)
-      );
-      $(elem).css("left", $(elem).attr("initleft") * 206 + 8);
-      $(elem).css("top", $(elem).attr("inittop") * 206 + 8);
+    $(elem).css({
+      "width" : $(elem).attr("data-tile-width") * 200 + (makeZero($(elem).attr("data-tile-width") - 1) * 6),
+      "height": $(elem).attr("data-tile-height") * 200 + (makeZero($(elem).attr("data-tile-height") - 1) * 6),
+      "left": $(elem).attr("initleft") * 206 + 8,
+      "top" : $(elem).attr("inittop") * 206 + 8
+    });
 
-      var closestElm = findClosest(this);
-      var tiles = getCovered(this);
+    var closestElm = findClosest(this);
+    var tiles = getCovered(this);
 
-        $(this).css("left", $(closestElm).position().left);
-        $(this).css("top", $(closestElm).position().top);
-        $(tiles.tiles).each(function(ind, elem){
-            $(elem).toggleClass("empty", false); //mark the tile we are now in as not empty
-            $(elem).css("z-index", "0");
-        });
-        $(this).css("z-index","1"); //set widget back on normal zindex
+    $(this).css({
+      "left": $(closestElm).position().left,
+      "top" : $(closestElm).position().top,
+      "z-index": "1"
+    });
+
+    $(tiles.tiles).each(function(ind, elem){
+      $(elem).css("z-index", "0")
+        .toggleClass("empty", false);
+    });
   });
 
   var held_element = {};
-  $('.widget').live('mousedown', function(e) { //when we pick up a tile
-      if(lock === true) {
-        held = false;
-        return false;
-      }
+  // When a tile is picked up
+  $(".widget").live("mousedown", function(e) {
+    if(lock === true) {
+      held_element.element = false;
+      return false;
+    }
 
-      held_element.offsetX = e.offsetX;
-      held_element.offsetY = e.offsetY;
+    held_element.offsetX = e.offsetX;
+    held_element.offsetY = e.offsetY;
+    held_element.oldX    = $(this).position().left;
+    held_element.oldY    = $(this).position().top;
 
-      if(e.preventDefault()){
-        e.preventDefault();
-      }
-
-      if( $(this).attr("data-app-source") === "from-drawer") {
-        $(this).clone().prependTo("body").css({
-            "left": $(this).offset().left,
-            "top" : $(this).offset().top,
-            "position": "absolute"
-        }).attr("mousedown", "true").attr({
-          "oldx": $(this).position().left,
-          "oldy": $(this).position().top
-        }).css("z-index","100");
-
-        $(".ui-2#apps,.ui-2#widgets").css("display", "none");
-      } else {
-        var tiles = getCovered(this);
-        $(tiles.tiles).each(function(ind, elem){
-          $(elem).toggleClass("empty", true);
-        });
-
-        $(this).attr("mousedown", "true").attr({
-          "oldx": $(this).position().left,
-          "oldy": $(this).position().top
-        }).css("z-index","100");
-      }
-
-      if(e.preventDefault()){
-        e.preventDefault();
-      }
-
-  });
-
-  $('.widget').live('mouseup', function(e) { //when we release a tile
-      if (lock === true) {
-        held = false;
-        return false;
-      }
-
-      if (held === false) {
-        return false;
-      }
-
-      $(".tile").removeClass("tile-green");
-      $(".tile").removeClass("tile-red");
-      $(".tile").css("z-index", "0");
-
-      $(this).removeClass("widget-drag");
-
-      $(this).attr("mousedown", "false");
-      update = true;
-
-      //find the closest tile
-      var closestElm = findClosest(this);
-      var tiles = getCovered(this);
-
-      if(tiles.clear === true){
-        if($(this).attr("data-app-source") === "from-drawer" && $(this).attr("data-widget") === "true") {
-
-          addWidget(true,
-            $(this).attr("id"),
-            $(closestElm).attr("data-land-top"),
-            $(closestElm).attr("data-land-left"),
-            $(this).attr("data-widget-src"),
-            $(this).attr("data-tile-width"),
-            $(this).attr("data-tile-height"),
-            $(this).attr("data-poke"),
-            stock_widgets[$(this).attr("data-stock")]
-          );
-        } else if( $(this).attr("data-app-source") === "from-drawer") {
-          addWidget(false,
-            $(this).attr("id"),
-            $(closestElm).attr("data-land-top"),
-            $(closestElm).attr("data-land-left"),
-            null, null, null, null, $(this).attr("data-stock")
-          );
-        } else {
-          updateWidget($(this).attr("id"),
-            $(closestElm).attr("data-land-top"),
-            $(closestElm).attr("data-land-left")
-          );
-        }
-
-        $(this).css("left", $(closestElm).position().left);
-        $(this).css("top", $(closestElm).position().top);
-        $(tiles.tiles).each(function(ind, elem){
-            $(elem).toggleClass("empty", false); //mark the tile we are now in as not empty
-            $(elem).css("z-index", "0");
-        });
-        $(this).css("z-index","1"); //set widget back on normal zindex
-
-
-      } else { //if the tile was full
-        $(tiles.tiles).each(function(ind, elem){
-            $(elem).css("z-index", "0");
-        });
-        $(this).css("left", parseInt( $(this).attr("oldx"), 10) );
-        $(this).css("top", parseInt( $(this).attr("oldy"), 10) );
-
-        $(this).css("z-index","1");
-
-        tiles = getCovered(this); // Recheck since it moved.
-        $(tiles.tiles).each(function(ind, elem){
-            $(elem).toggleClass("empty", false);
-        });
-        $(this).css("left", parseInt( $(this).attr("oldx"), 10) );
-        $(this).css("top", parseInt( $(this).attr("oldy"), 10) );
-      }
-
-      if( $(this).attr("data-app-source") === "from-drawer") {
-        $(this).remove();
-        // $(this).css("position", "").css("left", "").css("top", "");
-        // if( $(this).attr("data-widget") === "true") {
-        //   $(this).appendTo(".ui-2#widgets > .contents");
-        // } else {
-        //   $(this).appendTo(".ui-2#apps > .contents");
-        // }
-      }
-
-  });
-
-  var held = false;
-  $('body').live('mousemove', function(e) { // when we move a tile
-      if(lock === true) {
-        held = false;
-        return false;
-      }
-
-      held = false;
-      $(".widget").each(function(ind, elem){
-          if($(elem).attr("mousedown") == "true"){
-            held = elem;
-            return false;
-          }
+    if( $(this).attr("data-app-source") === "from-drawer") {
+      held_element.element = $(this).clone().prependTo("body")
+        .addClass("widget-drag").css({
+          "left": $(this).offset().left,
+          "top" : $(this).offset().top,
+          "position": "absolute",
+          "z-index" : "100"
       });
 
-      if( held !== false){
-        if(update === true){
-          update = false;
-          $(held).attr({
-            "lastx": e.pageX,
-            "lasty": e.pageY
-          });
-        }else{
-          $(held).css("left", e.pageX - held_element.offsetX);
-          $(held).css("top", e.pageY - held_element.offsetY);
-          $(held).attr("lastx", e.pageX);
-          $(held).attr("lasty", e.pageY);
-        }
+      // Ensure that it's always droppable
+      console.log( $(held_element.element).width()/2 )
+      held_element.offsetX = $(held_element.element).width()  / 2;
+      held_element.offsetY = $(held_element.element).height() / 2;
 
-        $(".edit-shortcut-ui").remove();
-        hscroll = true;
+      $(".ui-2#apps,.ui-2#widgets").css("display", "none");
+    } else {
+      var tiles = getCovered(this);
+      $(tiles.tiles).each(function(ind, elem){
+        $(elem).toggleClass("empty", true);
+      });
 
-        var closestElm = findClosest(held);
-        var tiles = getCovered(held);
+      $(this).addClass("widget-drag")
+        .css("z-index", "100");
 
-        $(".tile").removeClass("tile-green");
-        $(".tile").removeClass("tile-red");
-        $(".tile").css("z-index", "0");
-        $(held).addClass("widget-drag");
+      held_element.element = this;
+    }
 
-        if(tiles.clear === true){
-          $(tiles.tiles).each(function(ind, elem){
-              $(elem).addClass("tile-green");
-              $(elem).css("z-index", "2");
-          });
-        }else{
-          $(tiles.tiles).each(function(ind, elem){
-              $(elem).addClass("tile-red");
-              $(elem).css("z-index", "2");
-          });
-        }
+    if ( e.preventDefault ) {
+      e.preventDefault();
+    }
+  });
+
+  // When a tile is released
+  $(".widget").live("mouseup", function(e) {
+    if (lock === true) {
+      held_element.element = false;
+      return false;
+    }
+
+    if ( held_element.element === false ) {
+      return false;
+    }
+
+    update = true;
+
+    var closestElm = findClosest(this);
+    var tiles = getCovered(this);
+
+    if ( tiles.clear === true ) {
+      if( $(this).attr("data-app-source") === "from-drawer" && $(this).attr("data-widget") === "true" ) {
+        addWidget(true,
+          $(this).attr("id"),
+          $(closestElm).attr("data-land-top"),
+          $(closestElm).attr("data-land-left"),
+          $(this).attr("data-widget-src"),
+          $(this).attr("data-tile-width"),
+          $(this).attr("data-tile-height"),
+          $(this).attr("data-poke"),
+          stock_widgets[$(this).attr("data-stock")]
+        );
+      } else if( $(this).attr("data-app-source") === "from-drawer") {
+        addWidget(false,
+          $(this).attr("id"),
+          $(closestElm).attr("data-land-top"),
+          $(closestElm).attr("data-land-left"),
+          null, null, null, null, $(this).attr("data-stock")
+        );
+      } else {
+        updateWidget($(this).attr("id"),
+          $(closestElm).attr("data-land-top"),
+          $(closestElm).attr("data-land-left")
+        );
       }
+
+      $(this).removeClass("widget-drag").css({
+        "left": $(closestElm).position().left,
+        "top" : $(closestElm).position().top,
+        "z-index": "1"
+      });
+
+      $(tiles.tiles).each(function(ind, elem){
+          $(elem).toggleClass("empty", false);
+      });
+
+    } else { // If the tile was full
+
+      $(this).removeClass("widget-drag").css({
+        "left": held_element.oldX,
+        "top" : held_element.oldY,
+        "z-index": "1"
+      });
+
+      tiles = getCovered(this);
+
+      $(tiles.tiles).each(function(ind, elem){
+        $(elem).toggleClass("empty", false);
+      });
+    }
+
+    if( $(this).attr("data-app-source") === "from-drawer") {
+      $(this).remove();
+    }
+
+    held_element.element = false;
+    $(".tile").removeClass("tile-green tile-red")
+      .css("z-index", "0");
+  });
+
+  // When a tile is held and moved
+  $(document).live("mousemove", function(e) {
+    if(lock === true) {
+      held_element.element = false;
+      return false;
+    }
+
+    if ( typeof(held_element.element) === "object" ) {
+      if(update === true){
+        update = false;
+      }else{
+        $(held_element.element).css({
+          "left": e.pageX - held_element.offsetX - GRID_MARGIN_LEFT,
+          "top" : e.pageY - held_element.offsetY - GRID_MARGIN_TOP()
+        });
+      }
+
+      hscroll = true;
+
+      var closestElm = findClosest(held_element.element);
+      var tiles = getCovered(held_element.element);
+
+      $(".tile").removeClass("tile-green tile-red")
+        .css("z-index", "0");
+
+      if ( tiles.clear === true ) {
+        $(tiles.tiles).each(function(ind, elem){
+          $(elem).addClass("tile-green")
+            .css("z-index", "2");
+        });
+      } else {
+        $(tiles.tiles).each(function(ind, elem){
+          $(elem).addClass("tile-red")
+            .css("z-index", "2");
+        });
+      }
+    }
   });
 
 }
